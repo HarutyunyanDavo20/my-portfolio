7bb20ce66b60053c3868df8ca74f10412e28fc85
Davo <davs202012@mail.ru>
2023-12-05 19:39:01 +0600

    init

---
+++ client/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/davit-harutyunyan-748bb5239"
+        target="blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/HarutyunyanDavo20" target="blank">
+        <BsGithub />
+      </a>
+      <a href="https://www.instagram.com/harutyunyandavo20/" target="blank">
+        <BsInstagram />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
