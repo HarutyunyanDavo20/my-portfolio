59455a260dc33e1128eaeb376f0de630cb5689b6
Davo <davs202012@mail.ru>
2023-12-06 20:38:14 +0600

    auth

--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { AuthBody } from './types';
+import { Response } from 'express';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor() { }
+
+    @Post('/admin')
+    getAdmin(
+        @Res() res: Response,
+        @Body() body: AuthBody
+    ) {
+        if (body.login === process.env.AUTH_LOGIN && body.password === process.env.AUTH_PASSWORD) {
+            return res.status(200).json({ message: 'success' })
+        } else {
+            return res.status(401).json({ message: 'unauthorized' })
+        }
+    }
+}
